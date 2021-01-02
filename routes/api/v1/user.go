46d1e2335ea8ed47afbf93c93e910cfcf68c8eb9
Jecosine <mj_ol@outlook.com>
2021-01-03 07:59:42 +0800

    ADD: Add user.go

---
+++ routes/api/v1/user.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// GetAllUser Get all user from database
+func GetAllUser(c *gin.Context) {
+
+}
+
+// GetUser Get single user by specified id
+func GetUser(c *gin.Context) {
+
+}
+
+// AddUser add user
+func AddUser(c *gin.Context) {
+
+}
+
+// EditUser edit user
+func EditUser(c *gin.Context) {
+
+}
+
+// DeleteUser delete user
+func DeleteUser(c *gin.Context) {
+
+}
---
+++ routes/routes.go
@@ -0,0 +1,7 @@
+/*
+ * @Author: Jecosine
+ * @Date: 2021-01-03 07:53:33
+ * @LastEditTime: 2021-01-03 07:53:33
+ * @LastEditors: Jecosine
+ * @Description:
+ */
