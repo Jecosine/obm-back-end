2a4d2c3275fba585d1fed4c3127c33df047d7fa3
Jecosine <mj_ol@outlook.com>
2021-01-04 05:36:15 +0800

    ADD: Add category and tag models

---
+++ models/category.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: Jecosine
+ * @Date: 2021-01-04 02:45:03
+ * @LastEditTime: 2021-01-04 03:18:27
+ * @LastEditors: Jecosine
+ * @Description: model of category
+ */
+package models
+
+type Category struct {
+	Model
+}
---
+++ models/tag.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: Jecosine
+ * @Date: 2021-01-04 02:45:08
+ * @LastEditTime: 2021-01-04 04:16:19
+ * @LastEditors: Jecosine
+ * @Description: Tag model
+ */
+
+package models
